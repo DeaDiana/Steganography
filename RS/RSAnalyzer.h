5379fb6b4b18ea497db2d6bcab8b818502a6a626
Diana Anisutina <diana.anisutina@gmail.com>
2014-12-07 22:23:57 +0600

    Update RSAnalyzer.h
    Some reorganisation for functions.

--- RS/RSAnalyzer.h
+++ RS/RSAnalyzer.h
@@ -3,8 +3,25 @@
 
 #include <string>
 #include <exception>
+#include <map>
 
-typedef std::string GroupOfPixels;
+enum SIGN_MASK {
+	PLUS_MASK,
+	MINUS_MASK
+};
+
+enum MASK_VALUE {
+	MINUS_ONE,
+	ZERO,
+	PLUS_ONE
+};
+
+enum GROUPS {
+	REGULAR_M,
+	SINGULAR_M,
+	REGULAR_MM,
+	SINGULAR_MM
+};
 
 class RSAnalyzerException : public std::exception
 {
@@ -18,17 +35,37 @@ private:
 	std::string errorMessage;
 };
 
+
 class RSAnalyzer {
 public:
 	RSAnalyzer(void);
 	~RSAnalyzer(void);
-	bool IsStegoImage (const std::string & filename);
-	long lenghOfEmeddedMessge (const std::string & filename);
+	void setMask(const std::string & mask);
+	void setLengthOfMessage(const std::string & percent);
+	float lenghOfEmeddedMessge (const std::string & filename);
+	static void help();
 private:
-
-	long countDiscriminant(const GroupOfPixels & group);
-	GroupOfPixels flipGruopWithMask(const GroupOfPixels & group, const bool * mask);
-	GroupOfPixels flipGruop(const GroupOfPixels & group, const bool mask);
+	int header_sz;
+	long width;
+	long height;
+	int gray_value;
+	MASK_VALUE * mask;
+	int mask_length;
+	std::string filename;
+	std::string copyfilename;
+	std::string icopyfilename;
+	long file_sz;
+	float msg_percent;
+	long message_sz;
+	template <typename T> long countDiscriminant(const T * group);
+	short * flipGruopWithMask(char *  group, short *  fgroup, SIGN_MASK);
+	short flipByte(const char & byte, const MASK_VALUE mask_value);
+	void embedLSBMessage();
+	void checkFilePGMHeader(const std::string & filename);
+	template <typename T> 
+	T stringToNumber(const std::string& text);
+	std::map<GROUPS, float> countRSGroups(const std::string & filename);
+	void invertAllLSBs(const std::string & filename);
 };
 
 /* specification for PGM format
